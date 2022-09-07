146f7c004776b01829a802760a326088e9867c70
gab <gabrielmarianoofficial@gmail.com>
2022-09-07 23:13:09 +0800

    added changes to funders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,7 +2,23 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-  receive() external payable {
-    
-  }
+
+    address[] public funders; 
+
+    receive() external payable {}
+
+    function addFunds() public payable {
+      funders.push(msg.sender);
+    }
+
+    function getAllFunders() public view returns (address[] memory){
+      return funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns(address){
+      address [] memory _funders = getAllFunders();
+      return _funders[index];
+    }
 }
+
+// const instance = await Faucet.deployed()
