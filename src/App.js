93e80110eb0c5f21d2f736413d3507957ce4e9c3
gab <gabrielmarianoofficial@gmail.com>
2022-09-11 09:49:49 +0800

    improved UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import { useEffect, useState } from "react";
-import Web3 from "web3/dist/web3.min.js";
+import { useCallback, useEffect, useState } from "react";
+import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider";
+import { loadContract } from "./utils/load-contract";
 
 function App() {
   const [web3Api, setWeb3Api] = useState({
     provider: null,
+    isProviderLoaded: false,
     web3: null,
+    contract: null,
   });
 
+  const [balance, setBalance] = useState(null);
   const [account, setAccounts] = useState(null);
+  const [shouldReload, reload] = useState(false);
+
+  const reloadEffect = useCallback(() => reload(!shouldReload), [shouldReload]);
+
+  const setAccountListener = (provider) => {
+    provider.on("accountsChanged", (accounts) => window.location.reload());
+  };
 
   useEffect(() => {
     const loadProvider = async () => {
       const provider = await detectEthereumProvider();
 
       if (provider) {
-        setWeb3Api({ web3: new Web3(provider), provider });
+        const contract = await loadContract("Faucet", provider);
+        setAccountListener(provider);
+        setWeb3Api({ web3: new Web3(provider), provider, contract, isProviderLoaded: true});
       } else {
         console.error("Please install Metamask!");
       }
@@ -25,12 +38,44 @@ function App() {
   }, []);
 
   useEffect(() => {
+    const loadBalance = async () => {
+      const { contract, web3 } = web3Api;
+      let balance = await web3.eth.getBalance(contract.address);
+
+      setBalance(web3.utils.fromWei(balance, "ether"));
+    };
+
+    web3Api.contract && loadBalance();
+  }, [web3Api, shouldReload]);
+
+  useEffect(() => {
     const getAccount = async () => {
       const accounts = await web3Api.web3.eth.getAccounts();
       setAccounts(accounts[0]);
     };
     web3Api.web3 && getAccount();
   }, [web3Api.web3]);
+
+  const addFunds = useCallback(async () => {
+    const { contract, web3 } = web3Api;
+    await contract.addFunds({
+      from: account,
+      value: web3.utils.toWei("1", "ether"),
+    });
+
+    reloadEffect();
+  }, [web3Api, account, reloadEffect]);
+
+  const withdraw = async () => {
+    const { contract, web3 } = web3Api;
+    const withdrawAmount = web3.utils.toWei("0.1", "ether");
+
+    await contract.withdraw(withdrawAmount, {
+      from: account,
+    });
+
+    reloadEffect();
+  };
 
   return (
     <>
@@ -41,25 +86,44 @@ function App() {
             <h1>
               {account ? (
                 account
-              ) : (
+              ) : 
+                !web3Api.provider ? 
+                <>
+                  <div className="bg-yellow-300 rounded-md p-2 text-sm">
+                  Wallet is not detected!{` `}
+                  <a target="_blank" href="https://docs.metamask.io">
+                    Install Metamask
+                  </a>
+                  </div>
+                </> : 
+              (
                 <button
-                onClick={() => {
-                  web3Api.provider.request({method: "eth_requestAccounts"})
-                }} 
-                className="py-[2px] px-4 bg-slate-200 hover:bg-slate-300 rounded-md  duration-75 active:scale-[90%] ">
+                  onClick={() => {
+                    web3Api.provider.request({ method: "eth_requestAccounts" });
+                  }}
+                  className="py-[2px] px-4 bg-slate-200 hover:bg-slate-300 rounded-md  duration-75 active:scale-[90%] "
+                >
                   Connect Wallet
                 </button>
               )}
             </h1>
           </div>
           <div className="text-2xl md:text-4xl flex items-center justify-center gap-2">
-            Current Balance:<span className="font-bold">10</span> ETH
+            Current Balance:<span className="font-bold">{balance}</span> ETH
           </div>
           <div className="flex gap-4 my-5">
-            <button className="py-2 px-5 bg-blue-200 hover:bg-blue-300 rounded-md  duration-75 active:scale-[90%] ">
-              Donate
+            <button
+              disabled={!account}
+              onClick={addFunds}
+              className="py-2 px-5 bg-blue-200 hover:bg-blue-300 rounded-md  duration-75 active:scale-[90%] "
+            >
+              Donate 1 ETH
             </button>
-            <button className="py-2 px-5 bg-green-200 hover:bg-green-300 rounded-md duration-75 active:scale-[90%]">
+            <button
+              disabled={!account}
+              onClick={withdraw}
+              className="py-2 px-5 bg-green-200 hover:bg-green-300 rounded-md duration-75 active:scale-[90%]"
+            >
               Withdraw
             </button>
           </div>
